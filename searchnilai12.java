0b9f89c39c82e04925ab0fe4408c9bdbd8feb669
Jasmine Nasywa Nabilah <jasminenasywa6@gmail.com>
2024-10-31 20:54:14 +0700

    Jobsheet 9 no 4 pertanyaan

--- searchnilai12.java
+++ searchnilai12.java
@@ -3,19 +3,30 @@ public class searchnilai12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int [] arrnilai = {80, 85, 78, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        int a;
+        System.out.print("Masukkan jumlah index = ");
+        a = sc.nextInt();
+        int [] arrnilai = new int [a];
 
         for (int i = 0; i < arrnilai.length; i++) {
-            if (key == arrnilai[i]) {
-                hasil = i;
+            System.out.print("Masukkan nilai akhir ke-" + i + " : ");
+            arrnilai[i] = sc.nextInt();
+        }
+        System.out.print("Masukkan key = ");
+        int key = sc.nextInt();
+        int hasil = 0;
+
+        for (int j = 0; j < arrnilai.length; j++) {
+            if (key == arrnilai[j]) {
+                hasil = j;
                 break;
+            } if (key != arrnilai[j]){
+                System.out.println("Nilai tidak ditemukan");
             }
-    }
+    
     System.out.println();
     System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
     System.out.println();
 
     }
-}
+} }
