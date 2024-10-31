b27489242ae8b8c565c9fe8d8501b416e51c663f
Jasmine Nasywa Nabilah <jasminenasywa6@gmail.com>
2024-11-01 00:20:42 +0700

    Tugas no 2

--- tugas2job9.java
+++ tugas2job9.java
@@ -2,8 +2,32 @@ import java.util.Scanner;
 public class tugas2job9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-         
+
+        int hargatotal = 0;
+
+        System.out.print("Masukkan jumlah pesanan yang akan di pesan : ");
+        int a = sc.nextInt();
+        String[] pesanan = new String[a];
+        int [] harga = new int [a];
         
+        
+        for (int i = 0; i < a; i++){
+            System.out.print("Masukkan pesanan = ");
+            pesanan[i] = sc.nextLine();
+        }
+
+        for (int i = 0; i < a; i++) {
+            System.out.println("Masukkan harga " + pesanan[i] + "=");
+            harga[i] = sc.nextInt();
+            hargatotal += harga[i];
+        }
+        
+        System.out.println("-------------- Struk belanja -------------");
+        for (int i = 0; i < a; i++){
+            System.out.println("Pasanan " + pesanan[i] + " seharga Rp. " + harga[i]);
+        }
+        System.out.println("Total harga yang harus di bayar adalah Rp." + hargatotal);
+        System.out.println("Terimaksih sudah berkunjung.");
 
     }
 }
