a6bb0d0c0b6f7b91efb7aac67a633aa6ef189e28
Jasmine Nasywa Nabilah <jasminenasywa6@gmail.com>
2024-10-31 17:15:45 +0700

    Jobsheet 9 no 3

---
+++ arraynilairata12.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class arraynilairata12 {
+   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int [] nilaimhs = new int [10];
+    double total = 0;
+    double rata2;
+
+    for (int i = 0; i < nilaimhs.length; i++) {
+        System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
+        nilaimhs[i] = sc.nextInt();
+    }
+    for (int i = 0; i < nilaimhs.length; i++) {
+        total += nilaimhs[i];
+        
+    }
+    rata2 = total/nilaimhs.length;
+    System.out.println(("Rata-rata nilai = " + rata2));
+   } 
+}
