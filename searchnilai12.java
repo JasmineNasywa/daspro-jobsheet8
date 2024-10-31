34fe050ab643abca1a3407e519dbdeca0041660c
Jasmine Nasywa Nabilah <jasminenasywa6@gmail.com>
2024-10-31 18:19:49 +0700

    Jobsheet 9 no 4

---
+++ searchnilai12.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class searchnilai12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int [] arrnilai = {80, 85, 78, 90, 82, 86};
+        int key = 90;
+        int hasil = 0;
+
+        for (int i = 0; i < arrnilai.length; i++) {
+            if (key == arrnilai[i]) {
+                hasil = i;
+                break;
+            }
+    }
+    System.out.println();
+    System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
+    System.out.println();
+
+    }
+}
