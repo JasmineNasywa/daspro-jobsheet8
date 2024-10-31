21eaf36c9a697ced67df396bfc9d9bdfaa46a417
Jasmine Nasywa Nabilah <jasminenasywa6@gmail.com>
2024-10-31 17:53:22 +0700

    jobsheet9 no 3 pertanyaan

--- arraynilairata12.java
+++ arraynilairata12.java
@@ -2,10 +2,14 @@ import java.util.Scanner;
 public class arraynilairata12 {
    public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-
-    int [] nilaimhs = new int [10];
+    int a;
+    System.out.println("Masukkan jumlah mahasiswa-");
+    a = sc.nextInt();
+    int [] nilaimhs = new int [a];
     double total = 0;
     double rata2;
+
+    
 
     for (int i = 0; i < nilaimhs.length; i++) {
         System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
@@ -15,7 +19,14 @@ public class arraynilairata12 {
         total += nilaimhs[i];
         
     }
+    int j = 0;
+    for (int i = 0; i < nilaimhs.length; i++) {
+        if (nilaimhs[i] > 70) {
+            j++;
+        }
+    }
     rata2 = total/nilaimhs.length;
     System.out.println(("Rata-rata nilai = " + rata2));
+    System.out.println("Jumlah mahasiswa yang lulus adalah " + j);
    } 
 }
